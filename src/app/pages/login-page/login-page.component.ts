9a8c4360f52268a2ad61bcdd0505a7fc9924e819
221abdul <magomedow58426@gmail.com>
2024-08-15 18:41:54 +0300

    была проведене некая авторизация и добавлена FormGroup

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService = inject(AuthService)
 
+  profileForm = new FormGroup(
+    {
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required)
+    })
+    onSubmit(){
+      if(this.profileForm.valid){
+        //@ts-ignore
+        this.authService.login(this.profileForm.value)
+      }
+    }
 }
